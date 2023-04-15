69c73f016b5301faf3c1fa1859397e0026cc21d3
lkrum <leahkrumholz73@gmail.com>
2023-04-15 16:56:40 -0500

    added pseudocode to JS files

---
+++ Main/Home.js
@@ -0,0 +1,11 @@
+// need to connect to API data
+//  need function to get search button working
+  // need to save search input in local storage
+    // create search button variable
+    // set variable in localStorage.setItem("search-input", searchInput)
+  // need to save genre checkboxes in local storage
+    // create variable/array for each genre
+      // localStorage.setItem
+  // need to load results page when button is clicked
+    // location.assign(URL)
+  
---
+++ Main/SearchResults.js
@@ -0,0 +1,3 @@
+// need to pull search input and genre checkbox info from local storage and keep it on page
+  // getItem
+// need to integrate youtube API into search results
