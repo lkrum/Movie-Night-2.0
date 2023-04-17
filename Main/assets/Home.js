6ddc3f1b5b68405f8b9e2eae84d4ea7f6900e60b
lkrum <leahkrumholz73@gmail.com>
2023-04-17 14:17:28 -0500

    updated API key with new API were using

--- Main/assets/Home.js
+++ Main/assets/Home.js
@@ -10,10 +10,11 @@
     // location.assign(URL)
   
 // variable expressions
-var searchBtn = $('#searchBtn');
+// var searchBtn = $('#searchBtn');
+var genreOptions = []
 
 // variables
-var apiURL = 'moviesdatabase.p.rapidapi.com';
+var apiURL = 'streaming-availability.p.rapidapi.com';
 var apiKey = 'e23f87d15fmsh16684e11bab90f4p17d1eejsnca5dd7a7480b';
 
 // $('#searchBtn').click(getApi() {
@@ -30,11 +31,11 @@ const options = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': 'e23f87d15fmsh16684e11bab90f4p17d1eejsnca5dd7a7480b',
-    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
   }
 };
 
-fetch('https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856', options)
+fetch('https://streaming-availability.p.rapidapi.com/v2/search/title?title=star%20wars&country=us&show_type=movie&output_language=en' , options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
