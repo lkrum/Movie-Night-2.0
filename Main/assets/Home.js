a7b691f867d154542ec7fc062c5231c115d58685
lkrum <leahkrumholz73@gmail.com>
2023-04-17 11:19:32 -0500

    attempting javascript API code

--- Main/assets/Home.js
+++ Main/assets/Home.js
@@ -9,3 +9,32 @@
   // need to load results page when button is clicked
     // location.assign(URL)
   
+// variable expressions
+var searchBtn = $('#searchBtn');
+
+// variables
+var apiURL = 'moviesdatabase.p.rapidapi.com';
+var apiKey = 'e23f87d15fmsh16684e11bab90f4p17d1eejsnca5dd7a7480b';
+
+// $('#searchBtn').click(getApi() {
+//   fetch(requestUrl)
+//     .then(function (response) {
+//     return response.json();
+//   })
+//     .then(function (data) {
+//       console.log(data);
+// });
+// });
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'e23f87d15fmsh16684e11bab90f4p17d1eejsnca5dd7a7480b',
+    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+  }
+};
+
+fetch('https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
