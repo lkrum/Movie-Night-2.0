d75278ba8eb10fe4f784823711d40dce83d78f18
lkrum <leahkrumholz73@gmail.com>
2023-04-20 14:26:16 -0500

    worked on styling for recipe page and results page

--- Main/assets/recipes.js
+++ Main/assets/recipes.js
@@ -1,9 +1,9 @@
-var searchBtn = document.querySelector('#searchBtn')
-var searchInput = document.querySelector('#input-title');
+var searchRecipeBtn = document.querySelector('#searchRecipeBtn')
+var searchInput = document.querySelector('#input-recipe');
 var recipeSearch;
 
 // users input search terms for recipes and then go to separate results page
-searchBtn.addEventListener('click', function () {
+searchRecipeBtn.addEventListener('click', function () {
   recipeSearch = searchInput.value;
   localStorage.setItem('search-input', recipeSearch);
   return window.location.assign("./recipeResults.html");
