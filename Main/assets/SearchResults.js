b79187c7eb6a50d99f9eab203d9dfd61c9fb1dfd
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-04-20 14:37:43 -0500

    refactored SearchResults.js

--- Main/assets/SearchResults.js
+++ Main/assets/SearchResults.js
@@ -30,22 +30,25 @@ function titleGrab() {
 fetch('https://streaming-availability.p.rapidapi.com/v2/search/title?title=' + actualKeyword + '&country=us&show_type=movie&output_language=en', options)
 .then(response => response.json())
 .then(response => {
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 8; i++) {
+    var movieCard = document.getElementById('movie-' + i);
+
     // create and populate title element
     var titleText = document.createElement('h1');
-    document.body.appendChild(titleText);
+    movieCard.appendChild(titleText);
     var title = response.result[i].title;
     titleText.textContent = title;
     
     // create and populate IMDB rating element
     var ratingText = document.createElement('h2');
-    document.body.appendChild(ratingText);
+    movieCard.appendChild(ratingText);
     var rating = response.result[i].imdbRating;
     ratingText.textContent = 'IMDB Rating: ' + rating / 10 + '/10';
     
+    // create and populate streaming options elements and fixes capitalization
     var streamingOptionText = document.createElement('h2');
-    document.body.appendChild(streamingOptionText);
-    // undefined streaming options break loop. if they are undefined, page displays 'unavailable'
+    movieCard.appendChild(streamingOptionText);
+    // undefined streaming options break loop. if they are undefined, page displays 'Not Available'
     if (typeof response.result[i].streamingInfo.us !== 'undefined') {
       var streamingOption = response.result[i].streamingInfo.us;
       var streamingOptionKeyed = Object.keys(streamingOption);
@@ -59,17 +62,17 @@ fetch('https://streaming-availability.p.rapidapi.com/v2/search/title?title=' + a
     trailer.setAttribute("src", 'https://www.youtube.com/embed/' + youtubeId);
     trailer.setAttribute("width", "560");
     trailer.setAttribute("height", "315");
-    document.body.appendChild(trailer);
+    movieCard.appendChild(trailer);
 
     // create and populate overview element
     var overviewText = document.createElement('p');
-    document.body.appendChild(overviewText);
+    movieCard.appendChild(overviewText);
     var overview = response.result[i].overview;
     overviewText.textContent = overview;
 
     // create and populate movie poster element
     var moviePoster = document.createElement('img');
-    document.body.appendChild(moviePoster);
+    movieCard.appendChild(moviePoster);
     var primaryImageUrl = response.result[i].posterURLs.original;
     moviePoster.src = primaryImageUrl;
     moviePoster.style.maxWidth = '30vw';
@@ -78,6 +81,7 @@ fetch('https://streaming-availability.p.rapidapi.com/v2/search/title?title=' + a
     .catch(err => console.error(err));
 keywordEntry = '';
 }
+
 // gets genre selection from local storage
 // links api information and grabs
 function genreGrab() {
@@ -91,29 +95,30 @@ function genreGrab() {
 fetch('https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&services=netflix%2Cprime.buy%2Chulu.addon.hbo%2Cpeacock.free&output_language=en&show_type=movie&genre=' + genreChoice, options)
 .then(response => response.json())
 .then(response => {
-  for (var i = 0; i < 10; i++) {
-    console.log(genreChoice)
+  for (var i = 0; i < 8; i++) {
+    var movieCard = document.getElementById('movie-' + i);
+
     // create and populate title element
     var titleText = document.createElement('h1');
-    document.body.appendChild(titleText);
+    movieCard.appendChild(titleText);
     var title = response.result[i].title;
     titleText.textContent = title;
-    console.log(title)
     
     // create and populate IMDB rating element
     var ratingText = document.createElement('h2');
-    document.body.appendChild(ratingText);
+    movieCard.appendChild(ratingText);
     var rating = response.result[i].imdbRating;
     ratingText.textContent = 'IMDB Rating: ' + rating / 10 + '/10';
     
+    // create and populate streaming options elements and fixes capitalization
     var streamingOptionText = document.createElement('h2');
-    document.body.appendChild(streamingOptionText);
-    // undefined streaming options break loop. if they are undefined, page displays 'unavailable'
+    movieCard.appendChild(streamingOptionText);
+    // undefined streaming options break loop. if they are undefined, page displays 'Not Available'
     if (typeof response.result[i].streamingInfo.us !== 'undefined') {
       var streamingOption = response.result[i].streamingInfo.us;
       var streamingOptionKeyed = Object.keys(streamingOption);
       streamingOptionText.textContent = 'Streaming Locations: ' + streamingOptionKeyed;
-      } else streamingOptionText.textContent = 'Not Available';
+      } else streamingOptionText.textContent = 'Not Available'
 
     // create and populate youtube embed
     // !(ChatGPT)
@@ -122,17 +127,17 @@ fetch('https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&
     trailer.setAttribute("src", 'https://www.youtube.com/embed/' + youtubeId);
     trailer.setAttribute("width", "560");
     trailer.setAttribute("height", "315");
-    document.body.appendChild(trailer);
+    movieCard.appendChild(trailer);
 
     // create and populate overview element
     var overviewText = document.createElement('p');
-    document.body.appendChild(overviewText);
+    movieCard.appendChild(overviewText);
     var overview = response.result[i].overview;
     overviewText.textContent = overview;
 
     // create and populate movie poster element
     var moviePoster = document.createElement('img');
-    document.body.appendChild(moviePoster);
+    movieCard.appendChild(moviePoster);
     var primaryImageUrl = response.result[i].posterURLs.original;
     moviePoster.src = primaryImageUrl;
     moviePoster.style.maxWidth = '30vw';
